Cantera/cantera-svn

commit 5fe0b8b40975035c760ba239845841b45bb13ce3

Cleanup include statements
Move includes from header to implementation files where possible, and remove
unnecessary includes.

git-svn-id: https://cantera.googlecode.com/svn/cantera/trunk@3125 02a645c2-efd0-11dd-984d-ab748d24aa7e

src/base/ct2ctml.cpp
@@ -5,16 +5,12 @@
  */
 // Copyright 2001-2005  California Institute of Technology
 
-#include "cantera/base/ct_defs.h"
-#include "cantera/base/ctexceptions.h"
 #include "cantera/base/ctml.h"
-#include "cantera/base/global.h"
 #include "cantera/base/stringUtils.h"
 #include "../../ext/libexecstream/exec-stream.h"
 
 #include <fstream>
 #include <sstream>
-#include <functional>
 
 #ifdef _WIN32
 #include <windows.h>

commit 077d00c8ff9e2e2ce2af49892560915f8bd70c88

Fix compilation with Visual Studio 2013
Fixes regression introduced in r3125.

Resolves Issue 240.

git-svn-id: https://cantera.googlecode.com/svn/cantera/trunk@3208 02a645c2-efd0-11dd-984d-ab748d24aa7e

src/base/ct2ctml.cpp
@@ -11,6 +11,7 @@
 
 #include <fstream>
 #include <sstream>
+#include <functional>
 
 #ifdef _WIN32
 #include <windows.h>